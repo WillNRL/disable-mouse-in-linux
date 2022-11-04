1d3e7711c0e15b5e3ad5e920fba908a87eacc49a
will <will@pop-os.localdomain>
2022-11-04 14:24:57 -0400

    Second Commit

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 #Desativando o Mouse por lista de Device-ID
 echo "Desativando o Mouse por lista de Device-ID"
 xinput | grep -i mouse | grep -o [0-9][0-9] > temp-id-disable
-for counter in $(cat temp-id-disable); do xinput enable $counter; done 
+for counter in $(cat temp-id-disable); do xinput disable $counter; done 
 
 #Ativando o Mouse por lista de Device-ID
-##for counter in $(cat temp-id-disable); do xinput enable $counter; done 
+##for counter in $(cat temp-id-disable); do xinput enable $counter; done
